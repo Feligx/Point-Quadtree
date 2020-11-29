42f191cf2f55ef4c8e5b91f017e7bd5f761e1c05
Feligx <34454204+Feligx@users.noreply.github.com>
2020-11-29 13:35:13 -0500

    Update Quadtree.cpp - Actualización Quadtree.cpp
    Final Update of Quadtree.cpp.
    Actualización Final de Quadtree.cpp.
    
    Métodos añadidos, para el funcionamiento del sistema del revisión de colisiones, y limpieza de funciones obsoletas/inútiles.

--- Quadtree.cpp
+++ Quadtree.cpp
@@ -1,60 +1,56 @@
 #include "Quadtree.hpp"
 #include <iostream>
-//#include <SDL2/SDL.h>
+#include <math.h>
+
 
 using namespace std;
 
-template <typename T>
-QuadNode<T>::QuadNode(int x, int y, int w, int h , int c){
-    bounds.set_x(x);
-    bounds.set_y(y);
-    bounds.set_height(h);
-    bounds.set_width(w);
-    capacity = c;
-
-    parent = nullptr;
-    NW = nullptr;
-    NE = nullptr;
-    SW = nullptr;
-    SE = nullptr;
+QuadNode::QuadNode(int x_, int y_, int w_, int h_){
+    this->bounds = new Rectangle{ x_,y_,w_,h_};
+    this->NW = nullptr;
+    this->NE = nullptr;
+    this->SE = nullptr;
+    this->SW = nullptr;
 }
 
-template <typename T>
-Quadtree<T>::Quadtree(int x, int y, int w, int h , int c){
-    root= new QuadNode<T>(x,y,w,h,c);
-    capacity = c;
-}
+bool QuadNode::isLeaf(){return NW==nullptr;}
 
-template <typename T>
-bool QuadNode<T>::isLeaf(){return NW==nullptr;}
 
-template <typename T>
-bool QuadNode<T>::insert(Point<T> point){
-    if(!this->bounds->contains(point)){
+bool QuadNode::insert(Point point){//Método que inserta puntos en el árbol
+
+    if(!this->bounds->contain_point(point)){
+        //cout << "No contengo el punto :(" << endl;
         return false;
     }
 
-    if(isLeaf()){
+    if(isLeaf()){  //Si es una hoja el punto se inserta en este nodo
         if (count < capacity){
             this->data[count] = point;
+            cout << "agregare el punto: " << this->data[count].x << "," << this->data[count].y << endl;
             count++;
             return true;
         }
-        split();
+        split(); //Si el nodo alcanza su capacidad máxima se divide
     }
+
+    //Si no es una hoja o se divide, inserta el punto en el primer punto que le sea posible
     if(NW->insert(point)){
+        cout << "Logre insertarlo en NW" << endl;
         return true;
     }
     
     if(NE->insert(point)){
         return true;
+        cout << "Logre insertarlo en NW" << endl;
     }
     
     if(SW->insert(point)){
         return true;
+        cout << "Logre insertarlo en NW" << endl;
     }
     
     if(SE->insert(point)){
+        cout << "Logre insertarlo en NW" << endl;
         return true;
     }
     
@@ -63,38 +59,91 @@ bool QuadNode<T>::insert(Point<T> point){
     return false;
 }
 
-template <typename T>
-void QuadNode<T>::split(){
+void QuadNode::split(){
+    cout << "Spliting" << endl;
     int x= bounds->x;
     int y= bounds->y;
     int w= bounds->w;
     int h= bounds->h;
 
     //creacion de los 4 nodos hijos segun el limite de su respectivo cuadrante
-    NW= new QuadNode<T>( x , y , w/2 , h/2 );
-    NE= new QuadNode<T>( x + w/2 ,y ,w/2 ,h/2 );
-    SW= new QuadNode<T>( x , y + h ,w/2,h/2);
-    SE= new QuadNode<T>( x + w/2 , y + h/2 , w/2 , h/2 );
+    NW= new QuadNode( x , y , w/2 , h/2);
+    NE= new QuadNode( x+w/2, y, w/2, h/2);
+    SW= new QuadNode( x , y + h/2 ,w/2,h/2);
+    SE= new QuadNode( x + w/2 , y + h/2 , w/2 , h/2);
 
     //encuentra un hijo en el cual incluir cada punto de los que estan en data[]
     for(int i = 0; i < count; i++){
         if(NW->insert(data[i])){
+            cout << " Intentando NW! " << endl;
             continue;
         }
         if(NE->insert(data[i])){
+            cout << " Intentando NE! " << endl;
             continue;
         }
         if(SW->insert(data[i])){
+            cout << " Intentando SW! " << endl;
             continue;
         }
         if(SE->insert(data[i])){
+            cout << " Intentando SE! " << endl;
             continue;
         }
-        cout << "failed to place existing point after subdivide()" << endl;  
+        cerr << "failed to place existing point after subdivide()" << endl;  
+    }
+}
+
+float Point::distance(Point p) {
+    int d = 0; //distance
+    int x1 = this->x;
+    int y1 = this->y;
+    int x2 = p.x;
+    int y2 = p.y;
+
+    d = ((pow((x2 - x1), 2)) + (pow((y2 - y1), 2)));
+    return d;
+}
+
+
+bool Point::intersected(Point p) {
+    float distance= sqrt(this->distance(p)); //calcula la distancia entre los centros de los círculos
+    //cout << "The distance between the points is: "<<distance << endl;
+    int diameter = (this->radius)*2;
+    cout << "distance: "<<distance << endl;
+    cout << "diameter: " <<diameter << endl;
+    return distance <= diameter;
+}
+
+
+void QuadNode::query_colision(){
+    if(!isLeaf()){
+        this->NW->query_colision();
+        this->NE->query_colision();
+        this->SW->query_colision();
+        this->SE->query_colision();
+    }
+
+    else
+    {
+        //cout << "count: "<<this->count << endl;
+        for (int i = 0; i < this->count; i++) {
+            for (int j = 0; j < this->count; j++) {
+                //cout << j << endl;
+                //cout << this->data[i].intersected(this->data[j]) << " " << this->data[j].x << " " << this->data[j].y << " " << this->data[i].x << "," << this->data[i].y <<  endl;
+                if (this->data[i].intersected(this->data[j]) && this->data[j].x != this->data[i].x && this->data[j].y != this->data[i].y) { //Se revisa si el punto es diferente para no incluirse a si mismo y si este intersecta con los demás
+                    //query.push_back(this->data[j]);
+                    cout << "El punto: (" << this->data[i].x << "," << this->data[i].y << ") con radio:" << this->data[i].radius << "colisiona" << endl;
+                    cout << "con el punto: (" << this->data[j].x << "," << this->data[j].y << ") con radio:" << this->data[j].radius << "colisiona" << endl;
+                    this->data[j].highlighted = true;
+                }
+            }
+        }
     }
 }
 
 
-int main(){
-    return 0;
+
+void Quadtree::query() {
+    root->query_colision();
 }
